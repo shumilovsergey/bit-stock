c28f815378a38eb735f22fa53d0adb66c661dbef
shumilov <wumilovsergey@gmail.com>
2024-04-11 18:02:02 +0300

    up

--- api/views.py
+++ api/views.py
@@ -1,14 +1,23 @@
 from django.views import View
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 
 class Main(View):
     def get(self, request):
+        # token = request.session.get('token')
+        # print(token)
         return render(request, 'index.html')
     
+
+
+
+
 class Login(View):
     def get(self, request):
-        token= 0
-        print(type(request))
+        
+
+        token= "token exaple code 2"
+        request.session['token'] = token
 
         telegram_login_url = f"https://t.me/wget_bash_bot?start={token}"
         return render(request, 'login.html', {"telegram_login_url":telegram_login_url})
