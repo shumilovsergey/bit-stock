4b037001e56407d46571cf761263c3d3a3fd2427
shumilov <wumilovsergey@gmail.com>
2024-04-16 16:46:56 +0300

    up

--- api/models.py
+++ api/models.py
@@ -9,8 +9,59 @@ class TelegramUsers(models.Model):
 
     def __str__(self):
         return self.tg_id
-
     class Meta:
         ordering = ['-created']
+
+
+# class Org(models.Model):
+#     name = models.CharField(max_length=56)
+#     boss =  models.ForeignKey(TelegramUsers, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return self.name
+#     class Meta:
+#         ordering = ['-created']
+
+# class OrgEmployees():
+#     organisation = models.ForeignKey(Org, on_delete=models.CASCADE)
+#     employer = models.ForeignKey(TelegramUsers, on_delete=models.CASCADE)
+#     def __str__(self):
+#         return self.organisation
+#     class Meta:
+#         ordering = ['-created']
+
+# class OrgCategoris():
+#     organistatin = models.ForeignKey(Org, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=56)
+
+
+
+# !OrgPructs
+# name
+# category
+# description
+# photo? 
+# date_in
+# prcie_in
+# date_out
+# proce out
+
+
+#
+
+#// MiddleWare /organisation/<id>/
+# session.tg_id in Org.Emploers(filter organisation id)
+# if not => fck you page
+# if yes => show navbar col
+
+#// HTML templates
+# navbar row
+# navbar col
+
+#// webpage names
+#// webpage icons
+
+
+
+
 
 
